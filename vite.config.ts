13c4907464f355b0caba7fdf45a1079c75e8b4a7
xiaogao15228966 <107247185+xiaogao15228966@users.noreply.github.com>
2024-02-21 15:04:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+    publicPath: '/vue3/'
     plugins: [vue()],
     resolve: {
         alias: {
